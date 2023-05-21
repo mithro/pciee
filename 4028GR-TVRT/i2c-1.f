33d193d6252e8aedf8b0d0f9779ac5087f80238c
Tim 'mithro' Ansell <me@mith.ro>
2023-05-21 01:03:56 -0700

    Adding bus features output.

---
+++ 4028GR-TVRT/i2c-1.f
@@ -0,0 +1,16 @@
+Functionalities implemented by /dev/i2c-1:
+I2C                              no
+SMBus Quick Command              yes
+SMBus Send Byte                  yes
+SMBus Receive Byte               yes
+SMBus Write Byte                 yes
+SMBus Read Byte                  yes
+SMBus Write Word                 yes
+SMBus Read Word                  yes
+SMBus Process Call               yes
+SMBus Block Write                yes
+SMBus Block Read                 yes
+SMBus Block Process Call         yes
+SMBus PEC                        yes
+I2C Block Write                  yes
+I2C Block Read                   yes
